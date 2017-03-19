cab7680891d6914b322027afd074e12834100738
Chris Ng <chrisng@Chriss-MacBook-Pro.local>
2017-03-19 16:57:48 -0700

    refactor query for user sales/news

--- server/routes/me/meInfoController.js
+++ server/routes/me/meInfoController.js
@@ -5,23 +5,22 @@ import m from '../../models';
 
 async function getOwnNews(req, res) {
   try {
-    const { offset } = this.params;
+    let { offset } = req.params;
+    offset = parseInt(offset);
     const limit = 20;
-    let news = [];
     const user = await m.User.find({ where: { id: req.user.id }, include: [{ model: m.Brand }] });
-    for (let i = 0; i < user.Brands.length; i++) {
-      const query = {
-        where: { type: 'News' },
-        include: [
-          { model: m.Brand, where: { id: user.Brands[i].id } },
-          { model: m.Site },
-        ],
-        limit,
-        offset,
-      };
-      const relevantArticles = await m.Info.findAll(query);
-      news = news.concat(relevantArticles);
-    }
+    const brandIds = user.Brands.map(brand => brand.id);
+    const query = {
+      where: { type: 'News' },
+      include: [
+        { model: m.Brand, where: { id: { $in: brandIds } } },
+        { model: m.Site },
+      ],
+      order: 'date DESC',
+      limit,
+      offset,
+    };
+    const news = await m.Info.findAll(query);
     res.status(200).send({ success: true, news });
   } catch(err) {
     console.error(`Error retrieving news for user ${req.user.id}: ${JSON.stringify(err)}`);
@@ -31,23 +30,22 @@ async function getOwnNews(req, res) {
 
 async function getOwnSales(req, res) {
   try {
-    const { offset } = this.params;
+    let { offset } = req.params;
+    offset = parseInt(offset);
     const limit = 20;
-    let sales = [];
     const user = await m.User.find({ where: { id: req.user.id }, include: [{ model: m.Brand }] });
-    for (let i = 0; i < user.Brands.length; i++) {
-      const query = {
-        where: { type: 'Sale' },
-        include: [
-          { model: m.Brand, where: { id: user.Brands[i].id } },
-          { model: m.Site },
-        ],
-        limit,
-        offset,
-      };
-      const relevantArticles = await m.Info.findAll(query);
-      sales = sales.concat(relevantArticles);
-    }
+    const brandIds = user.Brands.map(brand => brand.id);
+    const query = {
+      where: { type: 'Sale' },
+      include: [
+        { model: m.Brand, where: { id: { $in: brandIds } } },
+        { model: m.Site },
+      ],
+      order: 'date DESC',
+      limit,
+      offset,
+    };
+    const sales = await m.Info.findAll(query);
     res.status(200).send({ success: true, sales });
   } catch(err) {
     console.error(`Error retrieving sales for user ${req.user.id}: ${JSON.stringify(err)}`);
