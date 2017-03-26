2cfa415f37478e0d21069141df24ef5437379683
Chris Ng <chrisng@Chriss-MacBook-Pro.local>
2017-03-26 15:10:52 -0700

    fix bug w/ brands in article item

--- app/src/components/ArticleItem.jsx
+++ app/src/components/ArticleItem.jsx
@@ -1,26 +1,53 @@
-import React, { PropTypes as T } from 'react';
+import React, { Component, PropTypes as T } from 'react';
 
 const propTypes = {
   article: T.object.isRequired,
 };
 
-export default function ArticleItem({ article }) {
-  const brandNames = article.Brands.map(brand => brand.name);
-  return (
-    <article className="article">
-      <section className="article-image-container">
-        <img src={article.imgUrl} />
-      </section>
-      <section className="article-info-container">
-        <header>
-          <a href={article.url} target="_blank">{article.title}</a>
-        </header>
-        <p className="article-info-container-date">{article.date}</p>
-        <p className="article-info-container-blurb">{article.blurb}</p>
+export default class ArticleItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brandNames: [],
+      renderBrandTag: false,
+    };
+    this.renderBrandTag = this.renderBrandTag.bind(this);
+  }
+
+  renderBrandTag() {
+    const { brandNames, renderBrandTag } = this.state;
+    if (renderBrandTag) {
+      return (
         <p className="article-info-container-brands">{brandNames.length > 1 ? 'Brands' : 'Brand'}: {brandNames.join(', ')}</p>
-      </section>
-    </article>
-  );
+      );
+    }
+    return null;
+  }
+
+  render() {
+    const { article } = this.props;
+    if (article.Brands) {
+      this.setState({
+        brandNames: article.Brands.map(brand => brand.name),
+        renderBrandTag: true,
+      });
+    }
+    return (
+      <article className="article">
+        <section className="article-image-container">
+          <img src={article.imgUrl} />
+        </section>
+        <section className="article-info-container">
+          <header>
+            <a href={article.url} target="_blank">{article.title}</a>
+          </header>
+          <p className="article-info-container-date">{article.date}</p>
+          <p className="article-info-container-blurb">{article.blurb}</p>
+          {this.renderBrandTag()}
+        </section>
+      </article>
+    );
+  }
 }
 
 ArticleItem.propTypes = propTypes;
