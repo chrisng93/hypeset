0e1db34be4f6a2db22774f4244f88676b7bdcea4
Chris Ng <chrisng@chriss-mbp.attlocal.net>
2017-03-25 21:35:39 -0700

    add brands to article

--- app/src/components/ArticleItem.jsx
+++ app/src/components/ArticleItem.jsx
@@ -5,17 +5,19 @@ const propTypes = {
 };
 
 export default function ArticleItem({ article }) {
+  const brandNames = article.Brands.map(brand => brand.name);
   return (
     <article className="article">
-      <section className="image-container">
+      <section className="article-image-container">
         <img src={article.imgUrl} />
       </section>
-      <section className="info-container">
+      <section className="article-info-container">
         <header>
           <a href={article.url} target="_blank">{article.title}</a>
         </header>
-        <p className="date">{article.date}</p>
-        <p className="blurb">{article.blurb}</p>
+        <p className="article-info-container-date">{article.date}</p>
+        <p className="article-info-container-blurb">{article.blurb}</p>
+        <p className="article-info-container-brands">{brandNames.length > 1 ? 'Brands' : 'Brand'}: {brandNames.join(', ')}</p>
       </section>
     </article>
   );
