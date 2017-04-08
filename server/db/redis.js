c19754b20732cfd74b73fb45b45e84552324992c
Chris Ng <chrisng@chriss-mbp.attlocal.net>
2017-04-08 02:01:28 -0700

    redis config for development

--- server/db/redis.js
+++ server/db/redis.js
@@ -7,7 +7,9 @@ import winston from 'winston';
 
 const logger = winston.loggers.get('redis');
 
-const client = Promise.promisifyAll(redis.createClient({ host: 'redis' }));
+const client = process.env.NODE_ENV === 'production' ?
+  Promise.promisifyAll(redis.createClient({ host: 'redis' })) :
+  Promise.promisifyAll(redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST));
 
 client.on('connect', () => logger.info('Connected to Redis', { action: 'connect' }));
 
