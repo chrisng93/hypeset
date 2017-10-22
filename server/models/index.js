2426350cc72b6b8ee54b63851e50e1cc5de68137
Chris Ng <chrisng93@gmail.com>
2017-10-22 11:33:05 -0400

    remove user

--- server/models/index.js
+++ server/models/index.js
@@ -27,49 +27,44 @@ pg.connect(pgConnectionString, (dbConnectError, client) => {
     if (createDbError && createDbError.code === '42P04') logger.debug('Database already exists', { action: 'attempted create' });
     if (!createDbError) logger.debug('Database created', { action: 'create' });
 
-    client.query(`CREATE USER ${DB_USER} SUPERUSER PASSWORD ${DB_PASSWORD}`, (createRoleError) => {
-      if (createRoleError) logger.debug('User already exists', { action: 'attempted create' });
-      if (!createRoleError) logger.debug('User created', { action: 'create' });
-
-      const sequelize = new Sequelize(pgConnectionString, {
-        host: DB_HOST,
-        port: DB_PORT,
-        dialect: 'postgres',
-        logging: false,
-      });
-
-      fs.readdirSync(__dirname)
-        .filter(file => (file.indexOf('.') !== 0) && (file !== basename))
-        .forEach((file) => {
-          if (file.slice(-3) !== '.js') return;
-          const model = sequelize['import'](path.join(__dirname, file));
-          db[model.name] = model;
-        });
-
-      Object.keys(db).forEach((modelName) => {
-        if (db[modelName].associate) {
-          db[modelName].associate(db);
-        }
-      });
-
-      db.sequelize = sequelize;
-      db.Sequelize = Sequelize;
-
-      sequelize
-        .sync()
-        .then(() => {
-          // populate table with Grailed and Hypebeast sites
-          db.Site.findOrCreate({ where: { name: 'Grailed' }, defaults: { name: 'Grailed', url: process.env.GRAILED_URL } });
-          db.Site.findOrCreate({ where: { name: 'Hypebeast' }, defaults: { name: 'Hypebeast', url: process.env.HYPEBEAST_URL } });
-          db.Site.findOrCreate({ where: { name: 'Reddit' }, defaults: { name: 'Reddit', url: process.env.REDDIT_URL } });
-
-          // create admin
-          db.User.findOrCreate({ where: { role: 'Admin' }, defaults: { username: process.env.ADMIN_USERNAME, password: process.env.ADMIN_PASSWORD, role: 'Admin' } });
-        })
-        .catch((err) => {
-          logger.warn('Error syncing Postgres models', { action: 'sync', err: JSON.stringify(err) });
-        });
+    const sequelize = new Sequelize(pgConnectionString, {
+      host: DB_HOST,
+      port: DB_PORT,
+      dialect: 'postgres',
+      logging: false,
     });
+
+    fs.readdirSync(__dirname)
+      .filter(file => (file.indexOf('.') !== 0) && (file !== basename))
+      .forEach((file) => {
+        if (file.slice(-3) !== '.js') return;
+        const model = sequelize['import'](path.join(__dirname, file));
+        db[model.name] = model;
+      });
+
+    Object.keys(db).forEach((modelName) => {
+      if (db[modelName].associate) {
+        db[modelName].associate(db);
+      }
+    });
+
+    db.sequelize = sequelize;
+    db.Sequelize = Sequelize;
+
+    sequelize
+      .sync()
+      .then(() => {
+        // populate table with Grailed and Hypebeast sites
+        db.Site.findOrCreate({ where: { name: 'Grailed' }, defaults: { name: 'Grailed', url: process.env.GRAILED_URL } });
+        db.Site.findOrCreate({ where: { name: 'Hypebeast' }, defaults: { name: 'Hypebeast', url: process.env.HYPEBEAST_URL } });
+        db.Site.findOrCreate({ where: { name: 'Reddit' }, defaults: { name: 'Reddit', url: process.env.REDDIT_URL } });
+
+        // create admin
+        db.User.findOrCreate({ where: { role: 'Admin' }, defaults: { username: process.env.ADMIN_USERNAME, password: process.env.ADMIN_PASSWORD, role: 'Admin' } });
+      })
+      .catch((err) => {
+        logger.warn('Error syncing Postgres models', { action: 'sync', err: JSON.stringify(err) });
+      });
   });
 });
 
